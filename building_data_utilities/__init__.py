f69bcc46423fc53b0ef0a26e784cf67527b925fd
Nicholas Long <1907354+nllong@users.noreply.github.com>
2025-10-21 12:09:13 -0600

    Remove deprecation warning (#39)
    * remove deprecation warning
    
    * remove downloading casa bonita

--- building_data_utilities/__init__.py
+++ building_data_utilities/__init__.py
@@ -2,9 +2,3 @@
 SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
 See also https://github.com/SEED-platform/building-data-utilities/blob/main/LICENSE.md
 """
-
-import warnings
-
-warnings.warn(
-    ("Importing 'building_data_utilities' is deprecated and will be removed in a future release."), DeprecationWarning, stacklevel=2
-)
